50b9efec9bab0cae0af2830501450cb9d115396c
Vladimir2ht <vovekus@mail.ru>
2023-07-03 02:49:16 +0300

    Working, not optimized variant.

--- site/index.php
+++ site/index.php
@@ -9,7 +9,45 @@ if (count($_GET) === 0) {
 	} else echo 'HTML-файл не найден.';
 	
 } else {
-	echo implode($_GET);
-}
+	
+	header("Content-Type: application/json");
 
-// echo implode($_GET);
+	function searchEntities($entity, $query) {
+		$nextPageUrl = "https://swapi.dev/api/{$entity}/";
+	
+		$results = [];
+	
+		while ($nextPageUrl) {
+			$response = file_get_contents($nextPageUrl);
+			$data = json_decode($response, true);
+	
+			$entities = array_filter($data['results'], function($e) use ($query) {
+				return stripos($e['name'], $query) !== false;
+			});
+	
+			$results = array_merge($results, $entities);
+			$nextPageUrl = $data['next'];
+		}
+	
+		return $results;
+	}
+
+	$query = $_GET['q'];
+
+	$people = searchEntities('people', $query);
+	$planets = searchEntities('planets', $query);
+	$ships = searchEntities('starships', $query);
+
+	echo json_encode([
+		'people' => array_map(function($el) {
+			return ['name' => $el['name'], 'gender' => $el['gender'], 'mass' => $el['mass']];
+		}, $people),
+		'planets' => array_map(function($el) {
+			return ['name' => $el['name'], 'population' => $el['population'], 'diameter' => $el['diameter']];
+		}, $planets),
+		'ships' => array_map(function($el) {
+			return ['name' => $el['name'], 'length' => $el['length'], 'crew' => $el['crew']];
+		}, $ships)
+	]);
+
+}
